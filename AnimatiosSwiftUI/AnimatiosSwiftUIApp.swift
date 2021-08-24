527c51ab63249fcf0acfe0b26b16317de02a62a8
DjNovovic <djordjijenovovic@gmail.com>
2021-08-24 18:28:05 +0200

    Initial Commit

---
+++ AnimatiosSwiftUI/AnimatiosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatiosSwiftUIApp.swift
+//  AnimatiosSwiftUI
+//
+//  Created by Đorđije Novović on 24.8.21..
+//
+
+import SwiftUI
+
+@main
+struct AnimatiosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
